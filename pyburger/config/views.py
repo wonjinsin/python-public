7a4d9588492fe711da79c83d317a81d4da486f45
Wonjinsin <slgktn92@gmail.com>
2023-05-27 22:41:54 +0900

    add. template

--- pyburger/config/views.py
+++ pyburger/config/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def main(request):
-    return HttpResponse("안녕하세요, pyburger입니다")
+    return render(request, "main.html")
 
 def burger_list(request):
-    return HttpResponse("pyburger의 햄버거 목록입니다.")
+    return render(request, "burger_list.html")
